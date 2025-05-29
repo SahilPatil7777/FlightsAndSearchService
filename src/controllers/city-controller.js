0ce7b1ac3ddfa63f4aa8f80e3215e798106fa5b5
Sahil Patil <sahilpatil32267@gmail.com>
2025-05-29 08:05:53 +0530

    Added get all cities api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -86,9 +86,30 @@ const update = async (req, res) => {
   }
 };
 
+const getAll = async (req, res) => {
+  try {
+    const cities = await cityService.getAllCities(req.query);
+    return res.status(200).json({
+      data: cities,
+      success: true,
+      message: "All City Fetched successfully",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to get all cities",
+      err: error,
+    });
+  }
+};
+
 module.exports = {
   create,
   destroy,
   get,
   update,
+  getAll,
 };
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,7 @@ const CityController = require('../../controllers/city-controller');
 const router = express.Router();
 router.post('/city', CityController.create);
 router.delete("/city/:id", CityController.destroy);
+router.get("/city", CityController.getAll); // Assuming you have a method to get all cities
 router.get("/city/:id", CityController.get);
 router.patch("/city/:id", CityController.update);
 
